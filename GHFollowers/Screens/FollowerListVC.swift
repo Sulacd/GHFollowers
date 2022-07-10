3d97ed5fb6f13bf3df7d82954f862612638ff420
Christian Diaz <sulacidi@gmail.com>
2022-07-10 15:11:42 -0700

    Implement the User Info View Controller

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -18,6 +18,7 @@ class FollowerListVC: UIViewController {
     var username: String!
     var page = 1
     var hasMoreFollowers = true
+    var isSearching = false
     
     var collectionView: UICollectionView!
     var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
@@ -57,7 +58,7 @@ class FollowerListVC: UIViewController {
                     }
                     return
                 }
-                
+                // Begins to dispaly the collection view once the data has been successfully retrived
                 self.updateData(on: self.followers)
                 
             case .failure(let errorMessage):
@@ -95,6 +96,7 @@ class FollowerListVC: UIViewController {
         dataSource = UICollectionViewDiffableDataSource<Section, Follower>(collectionView: collectionView, cellProvider: { collectionView, indexPath, follower in
             // Configures our collection view with a Follower Cell. The follower cell defines how to display the cell on the UI.
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FollowerCell.reuseID, for: indexPath) as! FollowerCell
+            // This is where the cell downloads the user icons 
             cell.set(follower: follower)
             return cell
         })
@@ -126,16 +128,31 @@ extension FollowerListVC: UICollectionViewDelegate {
             getFollowers(username: username, page: page)
         }
     }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let activeArray = isSearching ? filteredFollowers : followers
+        let follower = activeArray[indexPath.item]
+        
+        let destVC = UserInfoVC()
+        destVC.username = follower.login
+        
+        let navController = UINavigationController(rootViewController: destVC)
+        present(navController, animated: true)
+    }
 }
 
 extension FollowerListVC: UISearchResultsUpdating, UISearchBarDelegate  {
     func updateSearchResults(for searchController: UISearchController) {
+        // Dynamically captures a string that the user inputs
         guard let filter = searchController.searchBar.text, !filter.isEmpty else {return}
+        isSearching = true
+        // Filters out the followers array based on the filter string
         filteredFollowers = followers.filter {$0.login.lowercased().contains(filter.lowercased())}
         updateData(on: filteredFollowers)
     }
     
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        isSearching = false
         updateData(on: followers)
     }
 }
---
+++ GHFollowers/Screens/UserInfoVC.swift
@@ -0,0 +1,27 @@
+//
+//  UserInfoVC.swift
+//  GHFollowers
+//
+//  Created by Christian Diaz on 7/10/22.
+//
+
+import UIKit
+
+class UserInfoVC: UIViewController {
+    
+    var username: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
+        navigationItem.rightBarButtonItem = doneButton
+        
+        print(username!)
+    }
+    
+    @objc func dismissVC() {
+        dismiss(animated: true)
+    }
+
+}
